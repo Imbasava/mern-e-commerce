3654fecad9bcb6632317eb5cba5eddcc9feb860f
Basava <imbasavanayak@gmail.com>
2023-11-14 18:16:07 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+      <a href="/">ShopNow</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
